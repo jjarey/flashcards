56486f9ddcc5e0b74ca181d7d5787e0ad6f96ade
Jari Suni <jari.suni@G058017.local>
2022-11-21 11:26:35 +0200

    Added functionality for creating topics

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -3,11 +3,16 @@ import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+// import the useDispatch() method
+import { useDispatch } from "react-redux";
+// import addTopic
+import { addTopic } from "../features/topics/topicsSlice";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,6 +21,13 @@ export default function NewTopicForm() {
     }
 
     // dispatch your add topic action here
+    // include an id property by calling uuidv4(), the topic’s name, and an icon property
+    dispatch(addTopic({
+      id: uuidv4(),
+      name: name,
+      icon: icon
+    }));
+
     history.push(ROUTES.topicsRoute());
   };
 
